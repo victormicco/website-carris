0d293a294e4aae5a5fe758e5bb8859274c4d9f77
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-11-11 14:49:28 +0000

    Updated dark mode colors

--- frontend/app/(views)/(website)/alerts/[alert_id]/page.tsx
+++ frontend/app/(views)/(website)/alerts/[alert_id]/page.tsx
@@ -4,8 +4,20 @@ import AlertsDetail from '@/components/alerts/AlertsDetail';
 
 /* * */
 
-export default function Page({ params: { alert_id } }) {
+export default async function Page({ params }) {
+	//
+
+	//
+	// A. Setup variables
+
+	const { alert_id } = await params;
+
+	//
+	// B. Render components
+
 	return (
 		<AlertsDetail alertId={alert_id} />
 	);
+
+	//
 }
