6a9878fd9103724bb53ef654701277c0d98a9c65
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-11-22 15:34:08 +0000

    Add more redirects

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -44,6 +44,9 @@ const nextConfig = {
       { destination: 'https://backoffice.carrismetropolitana.pt/motoristas', permanent: false, source: '/motoristas' },
       //
       { destination: 'https://backoffice.carrismetropolitana.pt/novobanco', permanent: false, source: '/novobanco' },
+      { destination: 'https://backoffice.carrismetropolitana.pt/praias-area4', permanent: false, source: '/praias-area4' },
+      { destination: 'https://backoffice.carrismetropolitana.pt/freeport-pt', permanent: false, source: '/freeport-pt' },
+      { destination: 'https://backoffice.carrismetropolitana.pt/freeport-en', permanent: false, source: '/freeport-en' },
       //
     ]
   },
