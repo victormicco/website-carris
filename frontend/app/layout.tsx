92b05d392d8cb9fbc14b7ed02ebfc9b1d2c9bbdb
João de Vasconcelos <contact@joao.earth>
2025-01-27 12:44:47 +0000

    Enhance layout metadata type definition and update metadataBase handling

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 /* * */
 
 import { availableFormats } from '@/i18n/config';
+import { type Metadata } from 'next';
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
 import { Inter } from 'next/font/google';
@@ -21,9 +22,9 @@ const inter = Inter({
 
 /* * */
 
-export const metadata = {
+export const metadata: Metadata = {
 	description: 'Horários e Paragens em Tempo Real',
-	metadataBase: process.env.NEXT_PUBLIC_URL ? new URL(process.env.NEXT_PUBLIC_URL) : '',
+	metadataBase: process.env.NEXT_PUBLIC_URL ? new URL(process.env.NEXT_PUBLIC_URL) : null,
 	title: 'CMetropolitana',
 };
 
