a5d70fcfc08eedd96d7315ae5c5289666ffb6b81
João de Vasconcelos <contact@joao.earth>
2025-01-27 15:35:59 +0000

    Refactor OpenGraph handling: replace old image components with new implementation for dynamic image generation

--- frontend/app/(views)/(website)/viagem-2024/page.tsx
+++ frontend/app/(views)/(website)/viagem-2024/page.tsx
@@ -1,17 +1,17 @@
 /* * */
 
 import { Review2024Page } from '@/components/review-2024/Review2024Page';
-import { Metadata } from 'next';
+import { type Metadata } from 'next';
+
+/* * */
+
+export const metadata: Metadata = {
+	description: 'Explore a verdadeira dimensão da CMetropolitana em 2024.',
+	title: 'CMetropolitana | Viagem 2024',
+};
 
 /* * */
 
 export default function Page() {
 	return <Review2024Page />;
 }
-
-export const metadata: Metadata = {
-	openGraph: {
-		description: 'Viagens 2024',
-		title: 'CMetropolitana - Viagens 2024',
-	},
-};
---
+++ frontend/opengraph/OpenGraphReview2024Default.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+
+/* * */
+
+export function OpenGraphReview2024Default() {
+	return (
+		<div style={{ backgroundColor: '#fff', display: 'flex', height: '100%', width: '100%' }}>
+			<img
+				alt="Viagem 2024"
+				src={`${process.env.NEXT_PUBLIC_URL}/assets/review-2024/images/viagem2024logo.png`}
+				style={{ height: '100%', objectFit: 'contain', width: '100%' }}
+			/>
+		</div>
+	);
+}
