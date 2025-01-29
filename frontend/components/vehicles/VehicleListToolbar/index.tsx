335775f4a6077133be6643c98f2c97aa42f807fe
Skasix00 <brunoalexcastelo@gmail.com>
2025-01-29 10:29:50 +0000

    Added translations

--- frontend/components/vehicles/VehicleListToolbar/index.tsx
+++ frontend/components/vehicles/VehicleListToolbar/index.tsx
@@ -3,15 +3,12 @@
 /* * */
 
 import { FoundItemsCounter } from '@/components/common/FoundItemsCounter';
-import SortButton from '@/components/common/SortButton';
 import { Grid } from '@/components/layout/Grid';
 import { Section } from '@/components/layout/Section';
 import { Surface } from '@/components/layout/Surface';
 import { useStoresListContext } from '@/contexts/StoresList.context';
-import { SegmentedControl, Select } from '@mantine/core';
-import { IconMap } from '@tabler/icons-react';
+import { TextInput } from '@mantine/core';
 import { useTranslations } from 'next-intl';
-import { useMemo } from 'react';
 
 /* * */
 
@@ -21,39 +18,15 @@ export default function Component() {
 	//
 	// A. Setup variables
 
-	const t = useTranslations('stores.StoresListToolbar');
+	const t = useTranslations('vehicles.VehiclesListToolbar');
 	const storesContext = useStoresListContext();
 
 	//
 	// B. Transform data
 
-	const byCurrentStatusOptions = [
-		{ label: t('by_current_status.open', { count: storesContext.counters.by_current_status.open }), value: 'open' },
-		{ label: t('by_current_status.all'), value: 'all' },
-	];
-
 	const filterByOptions = [
-		{ label: t('filter_by.municipality_name'), value: 'municipality_name' },
-		{ label: t('filter_by.capacity'), value: 'capacity' },
-		{ label: t('filter_by.time'), value: 'wait_time' },
+		{ label: t('filter_by.search'), value: 'search' },
 	];
-
-	const byMunicipalityOptions = useMemo(() => {
-		if (!storesContext.data.raw) return [];
-		const uniqueMunicipalities = new Set();
-		const result: { disabled: boolean, label: string, value: string }[] = [];
-		storesContext.data.raw.forEach((store) => {
-			if (!uniqueMunicipalities.has(store.municipality_id)) {
-				uniqueMunicipalities.add(store.municipality_id);
-				result.push({
-					disabled: storesContext.data.raw.filter(item => item.municipality_id === store.municipality_id).length === 0,
-					label: store.municipality_name,
-					value: store.municipality_id,
-				});
-			}
-		});
-		return result.sort((a, b) => a.label.localeCompare(b.label));
-	}, [storesContext.data.raw]);
 
 	//
 	// C. Render components
@@ -61,15 +34,8 @@ export default function Component() {
 	return (
 		<Surface>
 			<Section heading={t('heading')} subheading={t('subheading')} withGap withPadding>
-				<SegmentedControl data={byCurrentStatusOptions} onChange={storesContext.actions.updateFilterCurrentStatus} value={storesContext.filters.by_current_status} w="100%" fullWidth />
-				<Grid columns="ab" withGap>
-					<Select data={byMunicipalityOptions} leftSection={<IconMap size={20} />} onChange={storesContext.actions.updateFilterByMunicipality} placeholder={t('by_municipality.label')} value={storesContext.filters.by_municipality} clearable searchable />
-					<SortButton
-						onDirectionChange={storesContext.actions.updateFilterOrderByDirection}
-						onOptionChange={storesContext.actions.updateFilterOrderBy}
-						options={filterByOptions}
-						selectedOption={storesContext.filters.order_by}
-					/>
+				<Grid columns="a" withGap>
+					<TextInput placeholder={t('filter_by.search')} />
 				</Grid>
 				<FoundItemsCounter text={t('found_items_counter', { count: storesContext.data.filtered.length })} />
 			</Section>
