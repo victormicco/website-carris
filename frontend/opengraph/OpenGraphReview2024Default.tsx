fd6781e6989c0978e115fd68d17b61ebb6a25eb7
João de Vasconcelos <contact@joao.earth>
2025-02-05 00:56:34 +0000

    Update OpenGraph image source to a static URL and remove conditional rendering

--- frontend/opengraph/OpenGraphReview2024Default.tsx
+++ frontend/opengraph/OpenGraphReview2024Default.tsx
@@ -5,13 +5,11 @@
 export function OpenGraphReview2024Default() {
 	return (
 		<div style={{ backgroundColor: '#fff', display: 'flex', height: '100%', width: '100%' }}>
-			{process.env.NEXT_PUBLIC_URL && (
-				<img
-					alt="Viagem 2024"
-					src={`${process.env.NEXT_PUBLIC_URL}/assets/review-2024/images/viagem-2024-logo.png`}
-					// style={{ height: '100%', objectFit: 'contain', width: '100%' }}
-				/>
-			)}
+			<img
+				alt="Viagem 2024"
+				src="https://www.carrismetropolitana.pt/assets/review-2024/images/viagem-2024-logo.png"
+				// style={{ height: '100%', objectFit: 'contain', width: '100%' }}
+			/>
 		</div>
 	);
 }
