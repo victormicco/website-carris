0a7d22cf22d8d8baa91376f98ec33883c476bfb9
João de Vasconcelos <contact@joao.earth>
2025-02-10 20:32:52 +0000

    Add Open Graph support for vehicle listings and update metadata

--- frontend/app/(views)/(website)/vehicles/layout.tsx
+++ frontend/app/(views)/(website)/vehicles/layout.tsx
@@ -1,6 +1,14 @@
 /* * */
 
 import { VehiclesListContextProvider } from '@/contexts/VehiclesList.context';
+import { type Metadata } from 'next';
+
+/* * */
+
+export const metadata: Metadata = {
+	description: 'Explore todos os veículos da CMetropolitana em tempo real.',
+	title: 'CMetropolitana | Frota',
+};
 
 /* * */
 
