6e8c54476312e8ddefcec7a3faa8806968f160a7
João de Vasconcelos <contact@joao.earth>
2025-02-11 23:07:00 +0000

    Remove test event capture from analytics enable action

--- frontend/components/cookies/Page/index.tsx
+++ frontend/components/cookies/Page/index.tsx
@@ -28,7 +28,6 @@ export default function Component() {
 
 	const handleEnable = () => {
 		analyticsContext.actions.enable();
-		analyticsContext.actions.capture('test');
 	};
 
 	const handleDisable = () => {
