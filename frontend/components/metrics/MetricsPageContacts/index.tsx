9c53233a9264e2730211c91b2764a858cfb4e246
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-03-07 18:03:40 +0000

    Added data filtering

--- frontend/components/metrics/MetricsPageContacts/index.tsx
+++ frontend/components/metrics/MetricsPageContacts/index.tsx
@@ -6,9 +6,13 @@ import { Surface } from '@/components/layout/Surface';
 import { MetricsPageContactsHeader } from '@/components/metrics/MetricsPageContactHeader';
 import { MetricsContactsPage2024CardGroup } from '@/components/metrics/MetricsPageContactsCardGroup';
 import { MetricsPageContactsToolbar } from '@/components/metrics/MetricsPageContactsToolbar';
+import { useLinesContext } from '@/contexts/Lines.context';
+import { useLocationsContext } from '@/contexts/Locations.context';
 import { Routes } from '@/utils/routes';
+import { Municipality } from '@carrismetropolitana/api-types/locations';
+import { Line } from '@carrismetropolitana/api-types/network';
 import { useTranslations } from 'next-intl';
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import useSWR from 'swr';
 
 import styles from './styles.module.css';
@@ -34,11 +38,10 @@ export function MetricsPageContacts() {
 	// A. Setup variables
 
 	const [filter_type, setFilterType] = useState<string>('global');
+	const [filter_value, setFilterValue] = useState<string>('-');
 	const [filtered_data, setFilteredData] = useState<Complaints[]>([]);
-
-	//
 	const t = useTranslations('metrics.MetricsPageContacts');
-
+	const linesContext = useLinesContext();
 	const { data: allComplaintsData } = useSWR(`${Routes.API}/metrics/complaints/`);
 
 	//
@@ -47,11 +50,14 @@ export function MetricsPageContacts() {
 	function handleDataSetup() {
 		if (!allComplaintsData) return;
 
-		const lineComplaints = allComplaintsData.filter(item => item.type === 'line');
-		const globalComplaints = allComplaintsData.filter(item => item.type === 'global');
-		const municipalComplaints = allComplaintsData.filter(item => item.type === 'municipality');
+		const lineComplaints = allComplaintsData.filter(item => item.type === 'line' && item.filter_value === filter_value);
 
-		console.log(municipalComplaints);
+		const municipalComplaints = allComplaintsData.filter(item => item.type === 'municipality' && item.filter_value === filter_value);
+
+		const globalComplaints = allComplaintsData.filter(item => item.type === 'global');
+
+		console.log('DAAAAAAM', municipalComplaints);
+		console.log('DAAAAAAM2', filter_type);
 
 		switch (filter_type) {
 			case 'global':
@@ -69,7 +75,7 @@ export function MetricsPageContacts() {
 
 	useEffect(() => {
 		handleDataSetup();
-	}, [allComplaintsData, filter_type]);
+	}, [allComplaintsData, filter_type, filter_value]);
 	//
 	// C. Handle Actions
 
@@ -77,12 +83,12 @@ export function MetricsPageContacts() {
 		if (value === filter_type && !value) {
 			setFilterType('global');
 		};
+
 		setFilterType(value);
 	}
 
 	//
 	// D. Render Components
-
 	return (
 		<Surface>
 			<div id="contactsMetrics">
@@ -91,9 +97,9 @@ export function MetricsPageContacts() {
 						<p>{t('text1')} </p>
 					</div>
 					<div className={styles.container}>
-						<MetricsPageContactsToolbar filter_type={handleFilterChange} />
+						<MetricsPageContactsToolbar allLines={linesContext.data.lines} filter_type={handleFilterChange} filter_value={setFilterValue} />
 						<MetricsPageContactsHeader data={filtered_data} />
-						<MetricsContactsPage2024CardGroup data={filtered_data} />
+						{/* <MetricsContactsPage2024CardGroup data={filtered_data} /> */}
 						{/* <MetricsContactsCards data={allComplaintsData} />
 					*/}
 					</div>
--- frontend/components/metrics/MetricsPageContactsToolbar/index.tsx
+++ frontend/components/metrics/MetricsPageContactsToolbar/index.tsx
@@ -1,52 +1,106 @@
 /* * */
 
 import Button from '@/components/common/Button';
+import { Municipality } from '@carrismetropolitana/api-types/locations';
+import { Line } from '@carrismetropolitana/api-types/network';
 import { Select } from '@mantine/core';
 import { useTranslations } from 'next-intl';
+import { useState } from 'react';
 
 import styles from './styles.module.css';
 /* * */
 
 interface Props {
-	filter_type: (filter) => void
-	// filter_value: string
+	allLines: Line[]
+	filter_type: (value) => void
+	filter_value: (value) => void
 }
 
-export function MetricsPageContactsToolbar({ filter_type, filter_value }: Props) {
+export function MetricsPageContactsToolbar({ allLines, filter_type, filter_value }: Props) {
 	//
 
 	//
 	// A. Setup variables
 	// const t = useTranslations('metrics.MetricsPageContactsSelector');
+	const [line, setLine] = useState<string>('');
+	const [municipality, setMunicipality] = useState<string>('');
 
-	// B. Fetch Data
+	const AML = [
+		{ label: 'Alcochete', value: '1520' },
+		{ label: 'Almada', value: '1503' },
+		{ label: 'Amadora', value: '1115' },
+		{ label: 'Barreiro', value: '1504' },
+		{ label: 'Cascais', value: '1105' },
+		{ label: 'Lisboa', value: '1106' },
+		{ label: 'Loures', value: '1107' },
+		{ label: 'Mafra', value: '1109' },
+		{ label: 'Moita', value: '1506' },
+		{ label: 'Montijo', value: '1507' },
+		{ label: 'Odivelas', value: '1116' },
+		{ label: 'Oeiras', value: '1110' },
+		{ label: 'Palmela', value: '1508' },
+		{ label: 'Seixal', value: '1510' },
+		{ label: 'Setúbal', value: '1512' },
+		{ label: 'Sintra', value: '1111' },
+		{ label: 'Vila Franca de Xira', value: '1114' },
+	];
 
+	// B. Handle Actions
+	const handleLineChange = (value) => {
+		if (municipality) {
+			setMunicipality('');
+		}
+
+		filter_type('line');
+		filter_value(value);
+		setLine(value);
+	};
+
+	const handleMunicipalityChange = (value) => {
+		if (line) {
+			setLine('');
+		}
+
+		filter_type('municipality');
+		filter_value(value);
+		setMunicipality(value);
+	};
+
+	const handleGlobalClick = () => {
+		if (municipality || line) {
+			setLine('');
+			setMunicipality('');
+		}
+		filter_value('-');
+		filter_type('global');
+	};
 	//
 	// C. Render Components
 	return (
 		<div className={styles.toolbarContainer}>
 			<Select
-				data={['React', 'Angular', 'Vue', 'Svelte']}
+				data={allLines.map(item => ({ label: `${item.id} - ${item.long_name}`, value: item.id }))}
 				label="Linhas"
-				onChange={() => filter_type('line')}
-				onClear={() => filter_type('global')}
+				onChange={_value => handleLineChange(_value)}
+				onClear={() => handleLineChange}
 				placeholder="Linhas"
+				value={line}
 				clearable
 				searchable
 			/>
 			<Select
-				data={['React', 'Angular', 'Vue', 'Svelte']}
+				data={AML.map(item => ({ label: item.label, value: item.value }))}
 				label="Municípios"
-				onChange={() => filter_type('municipality')}
-				onClear={() => filter_type('global')}
+				onChange={_value => handleMunicipalityChange(_value)}
+				onClear={() => handleMunicipalityChange}
 				placeholder="Municípios"
+				value={municipality}
 				clearable
 				searchable
 			/>
 			<Button
 				label="Geral"
-				onClick={() => filter_type('global')}
-
+				onClick={handleGlobalClick}
 			/>
 		</div>
 	);
