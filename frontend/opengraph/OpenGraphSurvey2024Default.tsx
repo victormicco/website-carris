e2109840079b0f4f11dd84db010a44c7b8ec2ffa
João de Vasconcelos <contact@joao.earth>
2025-03-10 10:23:47 +0000

    Comment out image element in OpenGraphSurvey2024Default component

--- frontend/opengraph/OpenGraphSurvey2024Default.tsx
+++ frontend/opengraph/OpenGraphSurvey2024Default.tsx
@@ -1,15 +1,13 @@
-/* eslint-disable @next/next/no-img-element */
-
 /* * */
 
 export function OpenGraphSurvey2024Default() {
 	return (
 		<div style={{ backgroundColor: '#fff', display: 'flex', height: '100%', width: '100%' }}>
-			<img
+			{/* <img
 				alt="Viagem 2024"
 				src="https://www.carrismetropolitana.pt/assets/survey-2024/images/survey-2024-logo.png"
 				// style={{ height: '100%', objectFit: 'contain', width: '100%' }}
-			/>
+			/> */}
 		</div>
 	);
 }
