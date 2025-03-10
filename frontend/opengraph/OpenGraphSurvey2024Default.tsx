084f74966ce0237b46456506cfc52b01813f0007
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-03-10 10:55:17 +0000

    Updated Image path

--- frontend/opengraph/OpenGraphSurvey2024Default.tsx
+++ frontend/opengraph/OpenGraphSurvey2024Default.tsx
@@ -7,7 +7,7 @@ export function OpenGraphSurvey2024Default() {
 		<div style={{ backgroundColor: '#fff', display: 'flex', height: '100%', width: '100%' }}>
 			<img
 				alt="Viagem 2024"
-				src="https://www.carrismetropolitana.pt/assets/survey-2024/images/survey-2024-logo.png"
+				src="https://www.carrismetropolitana.pt/assets/survey/images/survey-2024-logo.png"
 				// style={{ height: '100%', objectFit: 'contain', width: '100%' }}
 			/>
 		</div>
