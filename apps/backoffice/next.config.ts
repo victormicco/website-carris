cd4838e680aed2a47162b9b058e6b6a7678a3817
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-05-06 01:51:42 +0100

    Update Next.js configuration and package scripts for backoffice

--- apps/backoffice/next.config.ts
+++ apps/backoffice/next.config.ts
@@ -1,15 +1,15 @@
 /* * */
 
+import { withPayload } from '@payloadcms/next/withPayload';
 import { type NextConfig } from 'next';
-import { withPayload } from '@payloadcms/next/withPayload'
 
 /* * */
 
-const nextConfig = {
+const nextConfig: NextConfig = {
 	output: 'standalone',
 	reactStrictMode: true,
-}
+};
 
 /* * */
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload(nextConfig, { devBundleServerPackages: false });
