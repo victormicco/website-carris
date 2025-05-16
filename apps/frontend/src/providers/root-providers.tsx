4207756558c63dcb6d591e202f74c584fe4a238e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-05-16 02:30:57 +0100

    Wrap locale in a suspense boundary

--- apps/frontend/src/providers/root-providers.tsx
+++ apps/frontend/src/providers/root-providers.tsx
@@ -2,20 +2,24 @@
 
 /* * */
 
+import { AppLoader } from '@/components/common/AppLoader';
 import { AnalyticsContextProvider } from '@/contexts/Analytics.context';
 import { ConsentContextProvider } from '@/contexts/Consent.context';
 import { LocaleContextProvider } from '@/contexts/Locale.context';
+import { Suspense } from 'react';
 
 /* * */
 
 export function RootProviders({ children }) {
 	return (
-		<LocaleContextProvider>
-			<ConsentContextProvider>
-				<AnalyticsContextProvider>
-					{children}
-				</AnalyticsContextProvider>
-			</ConsentContextProvider>
-		</LocaleContextProvider>
+		<Suspense fallback={<AppLoader />}>
+			<LocaleContextProvider>
+				<ConsentContextProvider>
+					<AnalyticsContextProvider>
+						{children}
+					</AnalyticsContextProvider>
+				</ConsentContextProvider>
+			</LocaleContextProvider>
+		</Suspense>
 	);
 }
