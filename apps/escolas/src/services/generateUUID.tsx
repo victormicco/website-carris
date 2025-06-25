89d907f260a1aba5f547dbdf8949e1aefb62f02a
Classic-German <175220123+Classic-German@users.noreply.github.com>
2025-06-25 11:22:58 +0100

    Change generateUUID file

--- apps/escolas/src/services/generateUUID.tsx
+++ apps/escolas/src/services/generateUUID.tsx
@@ -5,7 +5,7 @@
 
 export default function generateUUID(dateObject) {
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-		let r = (Math.random() * 16) | 0,
+		const r = (Math.random() * 16) | 0,
 			v = c === 'x' ? r : (r & 0x3) | 0x8;
 		return v.toString(16);
 	});
